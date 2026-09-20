b954c3c5e60dbecbc38ae8780c271265bc80e4d1
pSujalp <sujalpdevarathnam@gmail.com>
2026-09-20 20:21:26 +0530

    all changes

---
+++ include/Mesh.h
@@ -0,0 +1,26 @@
+#pragma once 
+#include <vector>
+
+#include <simd/simd.h>
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
+#include "VertexData.hpp"
+
+using namespace simd;
+
+class Mesh{
+
+    public:
+
+    Mesh() = default;
+    Mesh( std::vector<VertexData> vertices, std::vector<uint32_t>  indices, MTL::Device * metalDevice);
+    
+
+    MTL::Buffer * VerticesBuffer;
+    MTL::Buffer * IndicesBuffer;
+
+
+
+
+
+};
---
+++ src/Mesh.cpp
@@ -0,0 +1,17 @@
+#include "Mesh.h"
+
+
+
+Mesh::Mesh(std::vector<VertexData> vertices, std::vector<uint32_t>  indices, MTL::Device * metalDevice){
+
+
+    VerticesBuffer = metalDevice->newBuffer(vertices.data(), sizeof(VertexData) * vertices.size(),MTL::ResourceStorageModeShared);
+    IndicesBuffer = metalDevice->newBuffer(indices.data(), sizeof(uint32_t) * indices.size(), MTL::ResourceStorageModeShared);
+
+    
+
+
+
+
+
+}
